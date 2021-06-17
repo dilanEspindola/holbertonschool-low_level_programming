b5c0afa059a2c9176937ed93c8d9b8d4f914b5e3
dilanEspindola <dilanespindola30@gmail.com>
2021-06-17 12:28:30 -0700

    archivo 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("the size of a char: %lu byte(s) \n", sizeof(char));
+printf("the size of an int: %lu bytes(s) \n", sizeof(int));
+printf("the size of a long int: %lu byte(s) \n", sizeof(long int));
+printf("the size of a long long: %lu byte(s) \n", sizeof(long long));
+printf("the size of a float: %lu byte(s) \n", sizeof(float));
+return (0);
+}
