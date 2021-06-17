40fb1756b86f572b13ffd5933258aa590ab13ca9
dilanEspindola <dilanespindola30@gmail.com>
2021-06-17 14:34:48 -0700

    archivo 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-
-/** 
-*main-initialcode  
-*Return:(0)
-*/
-
+/**
+ * main - Entry point
+ *
+ * Return: (0)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
