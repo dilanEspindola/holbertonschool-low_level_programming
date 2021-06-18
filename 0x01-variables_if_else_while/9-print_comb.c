5d5c61448d2660094141438b1fbb94a788855e29
dilanEspindola <dilanespindola30@gmail.com>
2021-06-18 14:53:20 -0700

    archivo 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - numbers 9 to 9 with commas and spaces
+ * main - numbers 0 to 9 with commas and spaces
  *
  * Return: (0)
  */
@@ -8,17 +8,15 @@ int main(void)
 {
 	int number = 47;
 
-	while (number <= 57)
+	while (number < 57)
 	{
 		number++;
 		putchar(number);
-		if (number == 57)
+		if (number != 57)
 		{
+			putchar(',');
 			putchar(' ');
-			break;
 		}
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
