f409a75240378febb54fe8703fbc2150767a62e3
dilanEspindola <dilanespindola30@gmail.com>
2021-06-18 14:06:32 -0700

    archivo 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - numbers 0 to 9
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
