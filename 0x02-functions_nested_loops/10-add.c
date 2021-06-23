5ea6e070a7f6877b4d1ab00b653a56155af3fe79
dilanEspindola <dilanespindola30@gmail.com>
2021-06-23 15:28:46 -0700

    archivo 10 - suma de dos numeros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - prints a + b
+ *
+ * @a: a is variable
+ *
+ * @b: b is a varaible
+ *
+ * Return: a + b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
