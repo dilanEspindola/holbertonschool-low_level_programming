b39b91cdc1e8f854e8de25956bea6f16ba7e5e95
dilanEspindola <dilanespindola30@gmail.com>
2021-06-24 12:18:45 -0700

    archivo 2 - multiplicacion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberotn.h"
+
+/**
+ * mul - multiplies two integeres
+ * @a: number integer
+ * @b: number integer
+ * Return: multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
