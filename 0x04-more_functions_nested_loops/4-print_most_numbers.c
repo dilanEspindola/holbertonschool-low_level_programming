a8406671fb786ebc5a09dbab7dca9651880e6874
dilanEspindola <dilanespindola30@gmail.com>
2021-06-24 13:27:22 -0700

    archivo 4 - imprime del 0 al 9 excepto el 2 y el 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9 except 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50)
+		{
+			continue;
+		}
+		else if (i == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
