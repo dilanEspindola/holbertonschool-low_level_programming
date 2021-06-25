17739e9d740de00116c5021cac9fc5459fd42322
dilanEspindola <dilanespindola30@gmail.com>
2021-06-25 09:34:03 -0700

    archivo 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ *
+ * @n: number integer
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
