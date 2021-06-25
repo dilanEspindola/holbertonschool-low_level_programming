4018ad99fb0fad8ad0dcc8c814f4d81d0976ac1d
dilanEspindola <dilanespindola30@gmail.com>
2021-06-25 14:36:29 -0700

    se modifico

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,9 +1,9 @@
-#ifndef holberton_h
-#define holberton_h
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
-#endif
+#endif /* HOLBERTON_H */
