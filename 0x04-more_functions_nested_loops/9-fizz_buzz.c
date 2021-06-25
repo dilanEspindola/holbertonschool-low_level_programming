91ee91ed316ccc20bb3fe89d38fc4a3cb16e2813
dilanEspindola <dilanespindola30@gmail.com>
2021-06-25 12:41:45 -0700

    archivo 9 - fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz
+ *
+ * Return: (0)
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+
+	return (0);
+}
