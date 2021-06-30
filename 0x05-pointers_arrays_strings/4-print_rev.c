c84b1da30b279034cdf6ba89afd0ee0c569ee6a3
dilanEspindola <dilanespindola30@gmail.com>
2021-06-29 18:39:11 -0700

    archivo 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: is a pointer
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
